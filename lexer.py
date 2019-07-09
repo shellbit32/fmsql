d344ad75aab587a9ccc30542e95e38ef913d1ac5
Fernando Rosendo <franplt629@gmail.com>
2019-07-09 16:14:15 -0300

    git ignore e lexer prontos

--- lexer.py
+++ lexer.py
@@ -2,3 +2,55 @@
 #                  Autores: Fernando Rosendo e Marcel Souza
 #                             Arquivo: "lexer.py"
 ################################################################################
+
+#Vamos definir nossos tokens neste arquivo
+
+from rply import LexerGenerator
+
+class Lexer():
+    def __init__(self):
+        self.lexer = LexerGenerator()
+
+    def _add_tokens(self):
+        #Select
+        self.lexer.add('SELECT',r'select')
+
+        #From
+        self.lexer.add('FROM',r'from')
+
+        #Where
+        self.lexer.add('WHERE',r'where')
+
+        #Separador
+        self.lexer.add('SEP',r',')
+
+        #Boxplot
+        self.lexer.add('BOXPLOT',r'boxplot')
+
+        #Strings alfanumericas
+        self.lexer.add('STR',r'\w+')
+
+#        #Col_select
+#        self.lexer.add('COL_SEL',r'\w+')
+#
+#        #data_from
+#        self.lexer.add('DAT_FR',r'\w+')
+#
+#        #Col_xbox
+#        self.lexer.add('COL_XBOX',r'\w+')
+#
+#        #Col_ybox
+#        self.lexer.add('COL_YBOX',r'\w+')
+
+        #Maior
+        self.lexer.add('GRT',r'>')
+
+        #Numero
+        self.lexer.add('NUM',r'\d')
+
+        #Ignorar espaços em branco
+        self.lexer.ignore('\s+')
+
+    def get_lexer(self):
+        self._add_tokens()
+        return self.lexer.build()
--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 #                  Autores: Fernando Rosendo e Marcel Souza
 #                             Arquivo: "main.py"
 ################################################################################
+
+#Combinando os componentes do compilador neste arquivo
+
+from lexer import Lexer
+
+text_input = """
+select col from base
+where col > 2
+boxplot col_x, col_y
+"""
+
+lexer = Lexer().get_lexer()
+tokens = lexer.lex(text_input)
+
+for token in tokens:
+    print(token)
